5e2155cedb421a3a445337d5cc553eb1a460b11c
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-03-05 16:08:50 +0900

    [ENGINE] 프로젝트가 이전에 초기화된 적이 있는지 검사하는 기능 추가

--- Engine/Script/Setup.py
+++ Engine/Script/Setup.py
@@ -6,14 +6,38 @@ def get_project_name_from_commandline():
     argv = sys.argv
 
     if len(argv) == 2:
-        project_name = argv[1]
-        print("setup {0}...".format(project_name))
+        return argv[1]
     else:
-        project_name = ""
-        print("please insert project name...")
+        return ""
     
-    return project_name
+# 프로젝트가 이미 초기화된 적이 있는지 확인합니다.
+def is_already_setup_project(project_directory):
+    project_elements = [
+        project_directory + "\\premake5.lua",
+        project_directory + "\\GenerateProjectFiles.bat",
+        project_directory + "\\Build.bat",
+        project_directory + "\\Game"
+    ]
+
+    for project_element in project_elements:
+        if os.path.exists(project_element):
+            return True
+
+    return False
+
 
 if __name__ == "__main__":
     project_directory = os.getcwd()
     project_name = get_project_name_from_commandline()
+
+    if not project_name:
+        print("usage Setup.bat <project-name>")
+        exit(0)
+
+    print("setup {0}...".format(project_name))
+
+    is_already_setup = is_already_setup_project(project_directory)
+
+    if is_already_setup:
+        print("already setup project...")
+        exit(0)
