97fe9e29fb4aba935d99b7afd921c7a5f23b7122
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-03-05 18:45:11 +0900

    [ENGINE] 명령행 인자의 옵션을 파싱하는 기능 추가

--- Engine/Script/Setup.py
+++ Engine/Script/Setup.py
@@ -1,6 +1,18 @@
 import os
 import sys
 
+def parse_commandline():
+    argvs = sys.argv
+    project_option = {}
+
+    for argv in argvs:
+        if not '=' in argv:
+            continue
+        argv_elements = argv.split('=')
+        project_option[argv_elements[0]] = argv_elements[1]
+
+    return project_option
+
 if __name__ == "__main__":
     project_directory = os.getcwd()
-    
+    project_option = parse_commandline()
