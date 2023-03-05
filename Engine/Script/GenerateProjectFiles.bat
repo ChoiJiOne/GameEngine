605495154b312fcd222b4a26300111ea92d03696
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-03-05 14:54:00 +0900

    [ENGINE] 크래시 덤프 파일의 저장 경로 수정
    - 기존의 CrashDump에서 Game\\Crash로 변경

--- Engine/Script/GenerateProjectFiles.bat
+++ Engine/Script/GenerateProjectFiles.bat
@@ -2,15 +2,14 @@
 
 SET project=%1
 SET visualstudio=%2
-SET crashdump=%3
 SET engine=Engine
 
 echo generate project files...
 
-if not exist %crashdump% (
-    echo can't find %crashdump% directory...
-    echo generate %crashdump% directory...
-    mkdir %crashdump%
+if not exist %project\\Game\\Crash% (
+    echo can't find %project\\Game\\Crash% directory...
+    echo generate %project\\Game\\Crash% directory...
+    mkdir %project\\Game\\Crash%
 )
 
 if exist %project% (
