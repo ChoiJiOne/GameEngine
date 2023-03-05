8ce9cf425ab3a1805a894b3b5ab9903496d53cb8
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2023-03-05 15:36:54 +0900

    [ENGINE] 명령행으로부터 프로젝트 이름 값을 얻는 기능 추가

--- Engine/Script/Setup.py
+++ Engine/Script/Setup.py
@@ -1,5 +1,19 @@
 import os
 import sys
 
+# 명령행으로부터 프로젝트의 이름값을 얻습니다.
+def get_project_name_from_commandline():
+    argv = sys.argv
+
+    if len(argv) == 2:
+        project_name = argv[1]
+        print("setup {0}...".format(project_name))
+    else:
+        project_name = ""
+        print("please insert project name...")
+    
+    return project_name
+
 if __name__ == "__main__":
-    print(os.getcwd())
+    project_directory = os.getcwd()
+    project_name = get_project_name_from_commandline()
