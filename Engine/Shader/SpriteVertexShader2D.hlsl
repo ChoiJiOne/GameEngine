778deaea10be1234a685394d3da335a8687fc7fc
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-03-09 12:49:34 +0900

    [ENGINE] 텍스처 렌더링의 회전 버그 수정 중 필요 없어진 World 행렬 삭제

--- Engine/Shader/SpriteVertexShader2D.hlsl
+++ Engine/Shader/SpriteVertexShader2D.hlsl
@@ -1,6 +1,5 @@
 cbuffer cbEveryFrame : register(b0)
 {
-    matrix World;
     matrix Projection;
 };
 
@@ -20,8 +19,7 @@ VS_OUTPUT main(VS_INPUT Input)
 {
     VS_OUTPUT Output;
     
-    Output.Position = mul(float4(Input.Position, 1.0f), World);
-    Output.Position = mul(Output.Position, Projection);
+    Output.Position = mul(float4(Input.Position, 1.0f), Projection);
     
     Output.UV = Input.UV;
     
