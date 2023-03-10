55cb216c4044f5e3627713409b35edd104fd67a8
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2023-03-10 14:09:31 +0900

    [ENGINE] AudioManager에 사운드를 생성 및 관리 기능 추가

--- Engine/Source/Private/AudioManager.cpp
+++ Engine/Source/Private/AudioManager.cpp
@@ -1,5 +1,7 @@
 #include "AudioManager.h"
 
+#include <algorithm>
+
 // @third party code - BEGIN
 #include <miniaudio/miniaudio.h>
 // @third party code - END
@@ -9,6 +11,7 @@ void AudioManager::Setup()
 	if (bIsSetup_) return;
 
 	CHECK((ma_engine_init(nullptr, MiniAudioEngine_) == MA_SUCCESS), "failed to initialize miniaudio engine...");
+	Sounds_.resize(0);
 
 	bIsSetup_ = true;
 }
@@ -26,6 +29,81 @@ void AudioManager::Cleanup()
 	MiniAudioEngine_ = nullptr;
 
 	bIsSetup_ = false;
+}
+
+int32_t AudioManager::CreateSound(const std::string& ResourcePath)
+{
+	ma_sound* Sound = nullptr;
+
+	CHECK((ma_sound_init_from_file(MiniAudioEngine_, ResourcePath.c_str(), 0, nullptr, nullptr, Sound) == MA_SUCCESS), "failed to cretae sound resource...");
+	Sounds_.push_back(Sound);
+
+	return CountSoundResource_++;
+}
+
+void AudioManager::SetSoundVolume(int32_t SoundID, float Volume)
+{
+	CHECK((0 <= SoundID && SoundID < Sounds_.size()), "out of range sound id...");
+
+	Volume = std::clamp<float>(Volume, 0.0f, 1.0f);
+	ma_sound_set_volume(Sounds_[SoundID], Volume);
+}
+
+float AudioManager::GetSoundVolume(int32_t SoundID)
+{
+	CHECK((0 <= SoundID && SoundID < Sounds_.size()), "out of range sound id...");
+
+	return ma_sound_get_volume(Sounds_[SoundID]);
+}
+
+void AudioManager::SetSoundLooping(int32_t SoundID, bool bIsLoop)
+{
+	CHECK((0 <= SoundID && SoundID < Sounds_.size()), "out of range sound id...");
+
+	ma_sound_set_looping(Sounds_[SoundID], static_cast<ma_bool32>(bIsLoop));
+}
+
+bool AudioManager::GetSoundLooping(int32_t SoundID)
+{
+	CHECK((0 <= SoundID && SoundID < Sounds_.size()), "out of range sound id...");
+
+	return ma_sound_is_looping(Sounds_[SoundID]) == MA_TRUE;
+}
+
+void AudioManager::PlaySound(int32_t SoundID)
+{
+	CHECK((0 <= SoundID && SoundID < Sounds_.size()), "out of range sound id...");
+	CHECK(ma_sound_start(Sounds_[SoundID]) == MA_SUCCESS, "failed to play sound");
+}
+
+bool AudioManager::IsPlayingSound(int32_t SoundID)
+{
+	CHECK((0 <= SoundID && SoundID < Sounds_.size()), "out of range sound id...");
+
+	return ma_sound_is_playing(Sounds_[SoundID]) == MA_TRUE;
+}
+
+bool AudioManager::IsDoneSound(int32_t SoundID)
+{
+	CHECK((0 <= SoundID && SoundID < Sounds_.size()), "out of range sound id...");
+
+	return ma_sound_at_end(Sounds_[SoundID]) == MA_TRUE;
+}
+
+void AudioManager::StopSound(int32_t SoundID)
+{
+	CHECK((0 <= SoundID && SoundID < Sounds_.size()), "out of range sound id...");
+	CHECK(ma_sound_stop(Sounds_[SoundID]) == MA_SUCCESS, "failed to stop play sound");
+}
+
+bool AudioManager::IsStoppingSound(int32_t SoundID)
+{
+	return !IsDoneSound(SoundID) && !IsPlayingSound(SoundID);
+}
+
+void AudioManager::ResetSound(int32_t SoundID)
+{
+	CHECK(ma_sound_seek_to_pcm_frame(Sounds_[SoundID], 0) == MA_SUCCESS, "failed to reset play sound");
 }
 
 AudioManager::~AudioManager()
--- Engine/Source/Public/AudioManager.h
+++ Engine/Source/Public/AudioManager.h
@@ -48,6 +48,134 @@ public:
 	void Cleanup();
 
 
+	/**
+	 * @brief 사운드 리소스를 생성합니다.
+	 * 
+	 * @param ResourcePath 사운드 리소스의 경로입니다.
+	 * 
+	 * @throws 
+	 * 사운드 파일 로딩에 실패하면 C++ 표준 예외를 던집니다.
+	 * 사운드 리소스 생성에 실패하면 C++ 표준 예외를 던집니다.
+	 * 
+	 * @return 생성된 사운드 리소스의 아이디를 반환합니다.
+	 */
+	int32_t CreateSound(const std::string& ResourcePath);
+
+
+	/**
+	 * @brief 사운드의 크기를 설정합니다.
+	 * 
+	 * @param SoundID 크기를 설정할 사운드의 아이디입니다.
+	 * @param Volume 사운드의 크기입니다. 범위는 0.0 ~ 1.0 입니다.
+	 * 
+	 * @throws SoundID에 대응하는 사운드 리소스가 없을 경우 C++ 표준 예외를 던집니다.
+	 */
+	void SetSoundVolume(int32_t SoundID, float Volume);
+
+
+	/**
+	 * @brief 현재 사운드의 크기를 얻습니다.
+	 * 
+	 * @param SoundID 소리 크기를 얻을 사운드의 아이디입니다.
+	 * 
+	 * @throws SoundID에 대응하는 사운드 리소스가 없을 경우 C++ 표준 예외를 던집니다.
+	 * 
+	 * @return 현재 사운드의 크기를 얻습니다.
+	 */
+	float GetSoundVolume(int32_t SoundID);
+
+
+	/**
+	 * @brief 사운드의 반복 여부를 설정합니다.
+	 * 
+	 * @param SoundID 반복 여부를 설정할 사운드의 아이디입니다.
+	 * @param bIsLoop 사운드 반복 여부입니다.
+	 * 
+	 * @throws SoundID에 대응하는 사운드 리소스가 없을 경우 C++ 표준 예외를 던집니다.
+	 */
+	void SetSoundLooping(int32_t SoundID, bool bIsLoop);
+
+
+	/**
+	 * @brief 사운드의 반복 여부를 얻습니다.
+	 * 
+	 * @param SoundID 반복 여부를 확인할 사운드의 아이디입니다.
+	 * 
+	 * @throws SoundID에 대응하는 사운드 리소스가 없을 경우 C++ 표준 예외를 던집니다.
+	 * 
+	 * @return 사운드가 반복한다면 true, 그렇지 않다면 false를 반환합니다.
+	 */
+	bool GetSoundLooping(int32_t SoundID);
+
+
+	/**
+	 * @brief 사운드를 플레이합니다.
+	 * 
+	 * @note 이전에 중지한 적이 있다면 중지한 시점부터 플레이됩니다.
+	 * 
+	 * @param SoundID 플레이할 사운드의 아이디입니다.
+	 * 
+	 * @throws SoundID에 대응하는 사운드 리소스가 없을 경우 C++ 표준 예외를 던집니다.
+	 */
+	void PlaySound(int32_t SoundID);
+
+
+	/**
+	 * @brief 사운드가 플레이중인지 확인합니다.
+	 * 
+	 * @param SoundID 플레이 중인지 확인할 사운드 아이디입니다.
+	 * 
+	 * @throws SoundID에 대응하는 사운드 리소스가 없을 경우 C++ 표준 예외를 던집니다.
+	 * 
+	 * @return 사운드가 플레이 중이라면 true, 그렇지 않다면 false를 반환합니다.
+	 */
+	bool IsPlayingSound(int32_t SoundID);
+
+
+	/**
+	 * @brief 사운드 플레이가 끝났는지 확인합니다.
+	 * 
+	 * @param 플레이가 끝났는지 확인할 사운드 아이디입니다.
+	 * 
+	 * @throws SoundID에 대응하는 사운드 리소스가 없을 경우 C++ 표준 예외를 던집니다.
+	 * 
+	 * @return 사운드 플레이가 끝났다면 true, 그렇지 않다면 false를 반환합니다.
+	 */
+	bool IsDoneSound(int32_t SoundID);
+
+
+	/**
+	 * @brief 사운드 플레이를 중지합니다.
+	 * 
+	 * @param SoundID 플레이를 중지할 사운드 아이디입니다.
+	 * 
+	 * @throws SoundID에 대응하는 사운드 리소스가 없을 경우 C++ 표준 예외를 던집니다.
+	 */
+	void StopSound(int32_t SoundID);
+
+
+	/**
+	 * @brief 사운드 플레이가 중지 되었는지 확인합니다.
+	 *
+	 * @param SoundID 플레이가 중지 되었는지 확인할 사운드 아이디입니다.
+	 * 
+	 * @throws SoundID에 대응하는 사운드 리소스가 없을 경우 C++ 표준 예외를 던집니다.
+	 * 
+	 * @return 사운드 플레이가 중지 되었다면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	bool IsStoppingSound(int32_t SoundID);
+
+
+	/**
+	 * @brief 사운드를 초기화합니다.
+	 * 
+	 * @param 초기화할 사운드의 아이디입니다.
+	 * 
+	 * @throws SoundID에 대응하는 사운드 리소스가 없을 경우 C++ 표준 예외를 던집니다.
+	 */
+	void ResetSound(int32_t SoundID);
+
+
 private:
 	/**
 	 * @brief 그래픽 관련 처리를 수행하는 클래스의 생성자입니다.
